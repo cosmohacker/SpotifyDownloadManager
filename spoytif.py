f7df415d25a60c6f7d6ecfea150250751e2f4dde
Yağızcan Yavuz <yagizcanyavuz@outlook.com>
2022-06-03 16:43:14 +0300

    Update spoytif.py

--- spoytif.py
+++ spoytif.py
@@ -2,32 +2,55 @@ import os
 import re
 import urllib.request
 import sys
-
+import spotdl
 import ijson
 import numpy as np
 from pytube import YouTube
+
+
+def downloadViaLink():
+    print("spotdl version :")
+    os.system('spotdl --version')
+
+    print("Please Choose Process Type [Download Song - 1 | Playlist - 2 ]: ")
+    processType = input()
+    if processType == '1':
+        print("Please Enter Song Name For Download : ")
+        songName = input()
+        print("Please Enter Download Location : ")
+        downloadLocation = input()
+        os.system('spotdl ' + "'" + songName + "'" + ' --output' + downloadLocation)
+    elif processType == '2':
+        print("Please Enter Spotify PlayList Link For Download : ")
+        playlistLink = input()
+        print("Please Enter Download Location : ")
+        downloadLocation = input()
+        os.system('spotdl ' + playlistLink + ' --output ' + downloadLocation)
+
+def downloadViaJson():
+    print("Please enter your .json file path EX:Path/file_name.json : ")
+    file_path = input()
+    json_filename = file_path
+    assert os.path.exists(file_path), "I did not find the file at, " + str(file_path)
+    f = open(file_path, 'r+')
+    f.close()
+    listAlbumNames(json_filename)
+
+    print("\nPlease type one of the above for download : ")
+    albumInput = input()
+    listName = albumInput
+
+    print("Please type download path : ")
+    download_path = input()
+
+    getTracks(listName, json_filename, download_path)
+
 
 def listAlbumNames(json_filename):
     with open(json_filename, 'rb') as data:
         for obj in ijson.items(data, 'playlists.item.name'):
             listName = obj
             print(listName)
-
-
-print("Please enter your .json file path EX:Path/file_name.json : ")
-file_path = input()
-json_filename = file_path
-assert os.path.exists(file_path), "I did not find the file at, " + str(file_path)
-f = open(file_path, 'r+')
-f.close()
-listAlbumNames(json_filename)
-
-print("\nPlease type one of the above for download : ")
-albumInput = input()
-listName = albumInput
-
-print("Please type download path : ")
-download_path = input()
 
 
 def jsonHierarchy(json_filename):
@@ -38,7 +61,7 @@ def jsonHierarchy(json_filename):
             print('prefix={}, event={}, value={}'.format(prefix, event, value))
 
 
-def getTracks(listName, json_filename):
+def getTracks(listName, json_filename, download_path):
     count = -1
     input = []
     with open(json_filename, 'rb') as file:
@@ -49,11 +72,11 @@ def getTracks(listName, json_filename):
                 # reset variables
                 value1 = value2 = 0
             elif event == 'end_map':
-               # reach the end of an object
+                # reach the end of an object
                 if value3 == listName:
                     count += 1
                     if isinstance(value1, int) and isinstance(value2, int):
-                        print ("Empty Track")
+                        print("Empty Track")
                     else:
                         input.insert(count, [value3, value1 + " " + value2])
                 continue
@@ -64,40 +87,29 @@ def getTracks(listName, json_filename):
             elif prefix.endswith('.name'):
                 value3 = value
 
-    # print("\nBefore : \n")
-
-    # for x in input:
-    # print(x, end="\n")
-
-    # print("\nAfter : \n")
-
     an_array = np.array(input)
     coordskeys = np.array(list(set([tuple(x) for x in an_array])))
-    # print(np.size(coordskeys))
-    # print(*coordskeys, sep=', ')
 
     for iy, ix in np.ndindex(coordskeys.shape):
         if ix != 0:
-            # print("\n" + coordskeys[iy, ix])
-            findVideoAndAdd(coordskeys[iy, ix])
+            findVideoAndAdd(coordskeys[iy, ix], download_path)
 
 
-def findVideoAndAdd(videoname):
-    # videoName = coordskeys[0, 1] + " " + coordskeys[0, 2]
+def findVideoAndAdd(videoname, download_path):
     videoName = videoname
     print(videoName)
     changeSpaces = videoName.replace(" ", "+")
-    unicode =''.join([i if ord(i) < 128 else '' for i in changeSpaces])
+    unicode = ''.join([i if ord(i) < 128 else '' for i in changeSpaces])
 
     html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + unicode)
     video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
     ytLink = "https://www.youtube.com/watch?v=" + video_ids[0]
     print(ytLink)
-    downloadFromYoutube(ytLink)
+    downloadFromYoutube(ytLink, download_path)
     # print("https://www.youtube.com/watch?v=" + video_ids[0])
 
 
-def downloadFromYoutube(youtubeURL):
+def downloadFromYoutube(youtubeURL, download_path):
     yt = YouTube(str(youtubeURL))
     video = yt.streams.filter(only_audio=True).first()
     destination = download_path
@@ -107,4 +119,13 @@ def downloadFromYoutube(youtubeURL):
     os.rename(out_file, new_file)
 
 
-getTracks(listName, json_filename)
+def chooseType():
+    print("Please Choose Process Type [Download Via Json - 1| Download Via Link/Name - 2]")
+    type = input()
+    if type == '1':
+        downloadViaJson()
+    elif type == '2':
+        downloadViaLink()
+
+
+chooseType()
